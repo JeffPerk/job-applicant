cfa99550248b998801803a5f6abe2782733cdb8c
Jeff Perkins <jeff.perk87@gmail.com>
2017-01-19 19:54:31 -0700

    added css tweaks

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -35,8 +35,8 @@
                   </ul>
               </li>
             @else
-              <li><a href="{{ url('/login') }}">Login</a></li>
-              <li><a href="{{ url('/register') }}">Register</a></li>
+              <li <?php echo ($_SERVER['REQUEST_URI'] === '/login') ? 'class="active"' : '';?>><a href="{{ url('/login') }}">Login</a></li>
+              <li <?php echo ($_SERVER['REQUEST_URI'] === '/register') ? 'class="active"' : '';?>><a href="{{ url('/register') }}">Register</a></li>
           @endif
         @endif
         </ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts/main')
 
 @section('content')
-  <h1>This is the welcome page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-header">
+            <h1 class="text-center">Welcome to the Applicant Tool</h1>
+          </div>
+          <hr>
+          <div class="panel-body">
+            <p class="text-center"><strong>This tool is to provide ease of access and to see what jobs are available!</strong></p>
+            <br><br>
+            <p class="text-center"><strong>This tool allows you to:</strong></p>
+            <ul class="list-group">
+              <li class="list-group-item text-center">View a clean preview of your profile</li>
+              <li class="list-group-item text-center">Edit your profile to always keep up to date</li>
+              <li class="list-group-item text-center">Admin can review all applicants</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
-
-        {{-- <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-        </div> --}}
